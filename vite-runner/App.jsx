c9c54f304093bc6d6a5334c64d62cd45b557daac
c0dedance <2627702283@qq.com>
2024-01-13 00:49:42 +0800

    feat: 使用 jsx

---
+++ vite-runner/App.jsx
@@ -0,0 +1,12 @@
+import React from './core/react'
+
+const App = (
+  <div id="app">Hello, mini react!</div>
+)
+// function App() {
+//   return (
+//     <div id="app">Hello, mini react!</div>
+//   )
+// }
+
+export default App
---
+++ vite-runner/main.jsx
@@ -0,0 +1,6 @@
+// import React from './core/react'
+import ReactDOM from './core/react-dom'
+import App from './App'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(App) // <App />
