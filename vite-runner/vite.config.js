4f24e132c7819248fddf7e15484c7612feeb26ec
c0dedance <2627702283@qq.com>
2024-01-13 12:25:11 +0800

    chore: close jsxRuntime

--- vite-runner/vite.config.js
+++ vite-runner/vite.config.js
@@ -1,15 +1,15 @@
-import { resolve } from 'node:path'
-import react from '@vitejs/plugin-react'
+// import { resolve } from 'node:path'
+// import react from '@vitejs/plugin-react'
 
 /** @type {import('vite').UserConfig} */
 export default {
   // 1. /** @jsx h */
   // 2. esbuild: { jsxFactory: 'h' }
   // 3. 自定义jsxRuntime
-  plugins: [
-    react({
-      jsxRuntime: 'automatic',
-      jsxImportSource: resolve('./core')
-    })
-  ],
+  // plugins: [
+  //   react({
+  //     jsxRuntime: 'automatic',
+  //     jsxImportSource: resolve('./core')
+  //   })
+  // ],
 }
