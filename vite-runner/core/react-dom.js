88a6ebd3dfd95d39f409166d40f1c4dde076d1cd
c0dedance <2627702283@qq.com>
2024-01-13 23:59:57 +0800

    feat: 实现 fiber 架构

--- vite-runner/core/react-dom.js
+++ vite-runner/core/react-dom.js
@@ -1,15 +1,88 @@
-function render(reactElement, container) {
-  const el = reactElement.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(reactElement.type)
-  const { children, ...props } = reactElement.props
-  Object.keys(props).forEach(key => {
-    el[key] = props[key]
-  })
+let nextWorkOfUnit = null
+function workloop(deadline) {
 
-  // 递归处理
-  children.forEach(child => render(child, el))
+  let shouldYield = false
 
-  container.append(el)
+  while (!shouldYield && nextWorkOfUnit) {
+    // run task
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
+    // update time
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workloop)
 }
+
+function createDom(type) {
+  return type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type)
+}
+function updateProps(dom, props) {
+  Object.keys(props).forEach(key => {
+    dom[key] = props[key]
+  })
+}
+
+function initChild(fiber) {
+  const { children } = fiber.props
+  let preChild
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      parent: fiber,
+      child: null,
+      sibling: null,
+      dom: null,
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      preChild.sibling = newFiber
+    }
+    preChild = newFiber
+  })
+}
+
+/* 构建Fiber结构并render */
+function performWorkOfUnit(fiber) {
+  const { children, ...props } = fiber.props
+
+  if (!fiber.dom) {
+    // 1. 创建dom
+    const dom = (fiber.dom = createDom(fiber.type))
+
+    // 2. 更新props
+    updateProps(dom, props)
+
+    fiber.parent.dom.append(dom)
+  }
+
+  // 3. 建立Fiber连接
+  initChild(fiber)
+
+  // 4. 返回下一个工作单元
+  if (fiber.child) {
+    return fiber.child
+  }
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+  // 返回undefined时表示处理完毕
+  return fiber.parent?.sibling
+
+
+}
+
+
+function render(reactElement, container) {
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [reactElement]
+    }
+  }
+}
+
+requestIdleCallback(workloop)
 
 const ReactDOM = {
   createRoot(container) {
@@ -20,6 +93,5 @@ const ReactDOM = {
     }
   }
 }
-
 
 export default ReactDOM
